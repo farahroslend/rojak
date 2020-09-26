55e2efb420f53306d72bb3d9a31257ed4361cd59
farahroslend <farahroslend@gmail.com>
2020-09-26 17:27:09 +0000

    connected Flaskt o MongoDB

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, 
+    request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Hello World"
+
+@app.route("/get_words")
+
+def words():
+    #defining words array from the mongodb collection words
+    words=mongo.db.words.find()
+    #words in blue is what the template will call the cluster of data from words in white
+    return render_template("dictionary.html", words=words)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
